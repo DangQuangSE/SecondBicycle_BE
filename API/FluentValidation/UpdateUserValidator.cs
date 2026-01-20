c7dd77555fbaf5717ebed46f50fb5780260b4c67
huuanh20 <165425332+huuanh20@users.noreply.github.com>
2026-01-20 19:02:05 +0700

    Validate users

---
+++ API/FluentValidation/UpdateUserValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Users;
+using FluentValidation;
+
+namespace API.FluentValidation
+{
+    public class UpdateUserValidator : AbstractValidator<UpdateUserDto>
+    {
+        public UpdateUserValidator()
+        {
+            RuleFor(x => x.RoleId)
+                .GreaterThan(0).WithMessage("Invalid role");
+
+            RuleFor(x => x.Status)
+                .InclusiveBetween((byte)0, (byte)1)
+                .WithMessage("Status must be 0 (Banned) or 1 (Active)");
+        }
+    }
+}
